8652a80afc4674098f4142d8f71ebc369d8714b5
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-08-19 15:30:15 -0400

    Refactor date creation code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // YYYY-MM-DD
 var ISO_DATE = /^\d{4}-\d{2}-\d{2}$/;
 
@@ -6,7 +8,7 @@ function LocalDate(value) {
     this.date = null;
   }
   else if (value instanceof Date) {
-    this.date = new Date(dateToLocalISODateString(value) + ' 00:00:00');
+    this.date = new Date(value.getFullYear(), value.getMonth(), value.getDate());
   }
   else if (typeof value === 'string' && ISO_DATE.test(value)) {
     this.date = new Date(value + ' 00:00:00');
@@ -17,7 +19,7 @@ function LocalDate(value) {
 
   return this;
 }
- 
+
 LocalDate.prototype.toString = function () {
   if (!this.date) {
     return '';
@@ -25,6 +27,8 @@ LocalDate.prototype.toString = function () {
 
   var date = this.date;
 
+  // Why not `date.toISOString()`? We want the date in local time, which should have a time of zero. If
+  // We convert to an ISO string, the timezone will convert to UTC, which may shift the date.
   var dateString = [date.getFullYear(), pad2(date.getMonth() + 1), pad2(date.getDate())].join('-');
 
   return dateString;
@@ -39,17 +43,6 @@ LocalDate.prototype.toFormattedString = function () {
 
   return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
 };
-
-/**
- * Convert a date object to an ISO date string without converting the timezone to UTC.
- * @param date {Date}
- * @returns {string} date portion of ISO date string (YYYY-MM-DD)
- */
-function dateToLocalISODateString(date) {
-  // Why not `date.toISOString()`? We want the date in local time, which should have a time of zero. If
-  // We convert to an ISO string, the timezone will convert to UTC, which may shift the date.
-  return [date.getFullYear(), pad2(date.getMonth() + 1), pad2(date.getDate())].join('-');
-}
 
 function pad2(number) {
   return number < 10 && number >= 0 ? '0' + number : number;
